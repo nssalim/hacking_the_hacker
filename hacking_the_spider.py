98758013aef77e483bf8873067c8a0ce4ff83547
Nargis <nargis.salim@btinternet.com>
2021-06-08 15:45:24 +0100

    reading in the passwords

--- hacking_the_spider.py
+++ hacking_the_spider.py
@@ -10,3 +10,31 @@
 # 1. acquire access to The Spider's systems
 # 2.  update The Spider's "passwords.txt" file to scramble the secret data.
 # 3.  add the signature of Glow Worm, a different hacker who could be halted by The Spider if they viewed Glow Worm as a threat.
+
+
+# Mission 1 - 
+# Reading In The Passwords
+# Write a program able to read compromised usernames and passwords stored in a file called "passwords.csv" on The Spider's machine.
+import csv
+
+ompromised_users = []
+with open('passwords.csv') as password_file:
+  password_csv = csv.DictReader(password_file)
+  for password_row in password_csv:
+    # print(password_row['Username'])
+# output
+# list of usernames (of each person whose password was compromised)
+
+# Add each username to the list of compromised_users.
+    compromised_users.append(password_row['Username'])
+    # print(compromised_users)
+# output
+# add username (of each person whose password was compromised) to list of compromised_users.
+
+# Write the username of each compromised_user in compromised_users to compromised_user_file
+with open ('compromised_users.txt', 'w') as compromised_user_file:
+  for compromised_user in compromised_users:
+    compromised_user_file.write(compromised_user)
+# output
+# compromised_users.txt file is created (and contains username of each compromise user)
+
