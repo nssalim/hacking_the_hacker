77d6beddb049901fda2aa34bcbfb3b41db361edc
Nargis <nargis.salim@btinternet.com>
2021-06-08 15:27:36 +0100

    first commit

---
+++ hacking_the_spider.py
@@ -0,0 +1,12 @@
+# Files
+# Hacking The Spider (computer hacker)
+# Work with Python files to  retrieve, manipulate, obscure, and create data.
+# work with CSV files and other text files.
+
+# Problem
+# The Spider, a computer hacker, has compromised several top-secret passwords.
+
+# Mission
+# 1. acquire access to The Spider's systems
+# 2.  update The Spider's "passwords.txt" file to scramble the secret data.
+# 3.  add the signature of Glow Worm, a different hacker who could be halted by The Spider if they viewed Glow Worm as a threat.
