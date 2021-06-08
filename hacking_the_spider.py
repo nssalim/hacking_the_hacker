8c007e6c8a373d68a8fbb3d1e57ac55505947137
Nargis <nargis.salim@btinternet.com>
2021-06-08 15:56:01 +0100

    notify the boss and scramble said message

--- hacking_the_spider.py
+++ hacking_the_spider.py
@@ -16,8 +16,9 @@
 # Reading In The Passwords
 # Write a program able to read compromised usernames and passwords stored in a file called "passwords.csv" on The Spider's machine.
 import csv
+import json
 
-ompromised_users = []
+compromised_users = []
 with open('passwords.csv') as password_file:
   password_csv = csv.DictReader(password_file)
   for password_row in password_csv:
@@ -38,3 +39,24 @@ with open ('compromised_users.txt', 'w') as compromised_user_file:
 # output
 # compromised_users.txt file is created (and contains username of each compromise user)
 
+
+# Notify the Boss
+# advise success in retrieving compromised data.
+# use JSON to encode said message over the internet.
+
+with open('boss_message.json', 'w') as boss_message:
+  boss_message_dict = {
+  "recipient":"The Boss", 
+  "message": "Mission Success"
+  }
+  json.dump(boss_message_dict, boss_message)
+# output
+# boss_message.json file is created (and content is:- 
+# {"recipient": "The Boss", 
+# "message": "Mission Success"})
+
+# Mission 2
+# Scrambling the Password
+# after safely recovering the compromised users, completely remove the "passwords.csv" file.
+with open("new_passwords.csv", "w") as new_passwords_obj:
+    
