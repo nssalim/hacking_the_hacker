dddaee682345a710fdfd1e2f4efa7a3ad7b27bcd
Nargis <nargis.salim@btinternet.com>
2021-06-08 16:34:39 +0100

    add contents of passwords.csv file

--- hacking_the_spider.py
+++ hacking_the_spider.py
@@ -6,13 +6,35 @@
 # Problem
 # The Spider, a computer hacker, has compromised several top-secret passwords.
 
+# passwords.csv
+# Username,Password
+# jean49,Da*E%OuGuc9$V1m
+# haydenashley,l$r^9eGg8f@BZhy
+# michaelastephens,$1sp++bga8H+eCr
+# denisephillips,Vj)T7AsfRHkfpvw
+# andrew24,T^mH8LMM&0C3VVk
+# kaylaabbott,!nN05pv3tc(DBO(
+# tmartinez,V46_Xx85_gKg7rt
+# mholden,(sd44x3x*A7D1dA
+# randygilbert,A7+E0YfB+MLPJ8Z
+# watsonlouis,i!4(DEkBLNq)C7G
+# mdavis,c$2VXHnxQ+6ifpx
+# patrickprice,Es_r0GlF&zDkJKG
+# kgriffith,%0dUpqgwFfA$Dma
+# hannasarah,c(9au%x)tyC#HDd
+# xaviermartin,e7!gWemxlde3K6p
+# hrodriguez,od@9P!dfQj8NH&A
+# erodriguez,hB+4A(si*vdHl4c
+# danielleclark,2lv3HKAs+Or8R48
+# timothy26,oz7ExOUI2&*S87h
+# elizabeth19,x3C8yYtI!(e3+z(
+
 # Mission
-# 1. acquire access to The Spider's systems
-# 2.  update The Spider's "passwords.txt" file to scramble the secret data.
-# 3.  add the signature of Glow Worm, a different hacker who could be halted by The Spider if they viewed Glow Worm as a threat.
+# 1. acquire access to The Spider's systems and read compromised usernames and passwords stored in a file called "passwords.csv"
+# 2. update The Spider's "passwords.csv" file to scramble the secret data.
+# 3. add the signature of Glow Worm, a different hacker who could be halted by The Spider if they viewed Glow Worm as a threat.
 
-
-# Mission 1 - 
+# Mission 1
 # Reading In The Passwords
 # Write a program able to read compromised usernames and passwords stored in a file called "passwords.csv" on The Spider's machine.
 import csv
@@ -59,7 +81,6 @@ with open('boss_message.json', 'w') as boss_message:
 # Scrambling the Password
 # after safely recovering the compromised users, completely remove the "passwords.csv" file.
 with open("new_passwords.csv", "w") as new_passwords_obj:
-
   
 # Mission 3
 # Hide trail and pin rival hacker, Glow Worm as person behind this attack. Add Glow Worm's signature:-
@@ -90,4 +111,5 @@ ____    ____
 # Write glow_worm_sig to new_passwords_obj. Now we have the file to replace passwords.csv with
   new_passwords_obj.write(glow_worm_sig)
 # output
-# new_password.csv file is created (and content is:- you got hacked gw
+# new_password.csv file is created (and content is:- you got hacked gw)
+
